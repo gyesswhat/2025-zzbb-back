785e024f852582a56155bd9c772bd92fe5865b9f
gyesswhat <h2weirdo@icloud.com>
2025-02-09 01:13:38 +0900

    chore: 초기 파일 세팅

---
+++ src/main/java/com/example/zzbb/ZzbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzbbApplication.class, args);
+	}
+
+}
